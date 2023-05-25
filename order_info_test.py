546cc78fbea45596cbeee920094670eb3c4bc745
eseniagit <134165594+eseniagit@users.noreply.github.com>
2023-05-25 21:50:54 +0300

    Update order_info_test.py

--- order_info_test.py
+++ order_info_test.py
@@ -2,7 +2,7 @@ import data
 import sender_stand_request
 
 def get_new_track_number(body):
-    order_response = sender_requests.create_new_order(body)
+    order_response = sender_stand_request.create_new_order(body)
     new_track_number = data.track_number.copy()
     new_track_number["t"] = order_response.json()["track"]
     return new_track_number
